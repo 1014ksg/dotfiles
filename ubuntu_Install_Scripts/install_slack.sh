ce305c58fb58be5a6466bcd0a5292cb24ecd7405
1014ksg <1014ksg1@keio.jp>
2020-09-22 14:20:55 +0900

    add:installer_slack

--- ubuntu_Install_Scripts/install_slack.sh
+++ ubuntu_Install_Scripts/install_slack.sh
@@ -2,5 +2,6 @@
 
 # slack
 ## wget コマンドと、ブラウザ上のdownloadpageから.deb package をdown loadする行為は同じこと！
+## deb（cashが残るのできををつける）
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
 sudo apt install ./slack-desktop-*.deb
